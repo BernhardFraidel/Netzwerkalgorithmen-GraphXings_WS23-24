11342ad2efa7036c47b9ea61ca501d64bcddb822
Bernhard Fraidel <bernhardfraidel@gmail.com>
2023-11-26 17:49:09 +0100

    shitty not winning but working Distance Player

--- src/GraphXings/solutions/DistancePlayer.java
+++ src/GraphXings/solutions/DistancePlayer.java
@@ -2,7 +2,6 @@ package GraphXings.solutions;
 
 import GraphXings.Algorithms.NewPlayer;
 import GraphXings.Algorithms.NewRandomPlayer;
-import GraphXings.Algorithms.RandomPlayer;
 import GraphXings.Data.Coordinate;
 import GraphXings.Data.Edge;
 import GraphXings.Data.Graph;
@@ -39,13 +38,28 @@ public class DistancePlayer implements NewPlayer {
     @Override
     public GameMove minimizeCrossings(GameMove lastMove) { return selectMove(lastMove); }
 
+    /**
+     * Calculate the distance between two coordinates
+     * @param a coordinate a
+     * @param b coordinate b
+     * @return distance between a and b
+     */
     public double distance(Coordinate a, Coordinate b) {
         double ac = Math.abs(b.getY() - a.getY());
         double cb = Math.abs(b.getX() - a.getX());
         return Math.hypot(ac, cb);
     }
 
-    private HashSet<Coordinate> sampleRandomGrid(int width, int height, int numHorizontalPartitions, int numVerticalPartitions, int[][] usedCoordinates) {
+    /**
+     * Get one random free coordinate from each tile in a imagined grid over the given playing field
+     *
+     * @param width                   width of the playing field
+     * @param height                  height of the playing field
+     * @param numHorizontalPartitions horizontal granularity of the gird
+     * @param numVerticalPartitions   vertical granularity of the grid
+     * @return Set of free coordinates, one coordinate for each tile in the grid.
+     */
+    private HashSet<Coordinate> sampleRandomGrid(int width, int height, int numHorizontalPartitions, int numVerticalPartitions) {
         // make sure that partitions size will be bigger or equal to 1
         if (width <= numHorizontalPartitions) {
             numHorizontalPartitions = width;
@@ -73,7 +87,7 @@ public class DistancePlayer implements NewPlayer {
                 if (yUpper >= height) {
                     yUpper = height - 1;
                 }
-                Optional<Coordinate> gridCoordinate = getRandomGridCoordinate(xLower, xUpper, yLower, yUpper, usedCoordinates);
+                Optional<Coordinate> gridCoordinate = getRandomGridCoordinate(xLower, xUpper, yLower, yUpper);
                 // skip if no free coordinate is found
                 if (gridCoordinate.isEmpty()) continue;
                 randomGridCoordinates.add(gridCoordinate.get());
@@ -82,7 +96,12 @@ public class DistancePlayer implements NewPlayer {
         return randomGridCoordinates;
     }
 
-    private Optional<Coordinate> getRandomGridCoordinate(int xLower, int xUpper, int yLower, int yUpper, int[][] usedCoordinates) {
+    /**
+     * given a range of coordinates return one random coordinate in the range that is still free.
+     * If there are no more free coordinates empty will be returned
+     * @return optionally empty random coordinate
+     */
+    private Optional<Coordinate> getRandomGridCoordinate(int xLower, int xUpper, int yLower, int yUpper) {
         int x;
         int y;
         Coordinate tileCoordinate = null;
@@ -93,7 +112,7 @@ public class DistancePlayer implements NewPlayer {
             x = Math.toIntExact(r.nextInt(xUpper + 1 - xLower) + xLower);
             y = Math.toIntExact(r.nextInt(yUpper + 1 - yLower) + yLower);
             tries++;
-            if (usedCoordinates[x][y] == 0) {
+            if (gameState.getUsedCoordinates()[x][y] == 0) {
                 tileCoordinate = new Coordinate(x, y);
                 break;
             }
@@ -103,30 +122,60 @@ public class DistancePlayer implements NewPlayer {
     }
 
     /**
-     *
-     * @return get the neighbors to all placed vertices with a list to the already placed vertices that are connected to the neighbor
+     * For all placed Vertices P get the Neighbourhood N(P) where n in N(P), but not in P
+     * @return Map every vertex p in P to its Neighbourhood N(p)
      */
-    private HashMap<Vertex, List<Vertex>> getNeighborsOfPlacedVertices (){
+    private HashMap<Vertex, List<Vertex>> getFreeNeighborsOfPlacedVertices(){
         HashMap<Vertex,List<Vertex>> neighborsOfPlacedVertices = new HashMap<>();
+        // TODO output wieder entfernen oder besser machen
+        if (gameState.getPlacedVertices().isEmpty()) System.out.println("There are no placed vertices to get neighbours");
+
         Iterator<Vertex> placedVertices = gameState.getPlacedVertices().iterator();
         while(placedVertices.hasNext()){
             Vertex v = placedVertices.next();
             Iterable<Edge> incidentEdges = g.getIncidentEdges(v);
             for(Edge e : incidentEdges){
                 Vertex n = e.getS() == v ? e.getT(): e.getS();
-                neighborsOfPlacedVertices.get(v).add(n);
+                if (!gameState.getPlacedVertices().contains(n)) neighborsOfPlacedVertices.get(v).add(n);
             }
         }
         return neighborsOfPlacedVertices;
     }
 
     private GameMove randomMove(GameMove lastMove){
-        NewRandomPlayer newRandomPlayer = new NewRandomPlayer("rand");
-        GameMove move = switch(role){
-            case MAX -> newRandomPlayer.maximizeCrossings(lastMove);
-            case MIN -> newRandomPlayer.minimizeCrossings(lastMove);
-        };
-        return move;
+        //NewRandomPlayer newRandomPlayer = new NewRandomPlayer("rand");
+        //newRandomPlayer.initializeNextRound(g,width,height,role);
+        //GameMove move = switch(role){
+        //    case MAX -> newRandomPlayer.maximizeCrossings(lastMove);
+        //    case MIN -> newRandomPlayer.minimizeCrossings(lastMove);
+        //};
+        //return move;
+
+        int stillToBePlaced = g.getN()- gameState.getPlacedVertices().size();
+        int next = r.nextInt(stillToBePlaced);
+        int skipped = 0;
+        Vertex v=null;
+        for (Vertex u : g.getVertices())
+        {
+            if (!gameState.getPlacedVertices().contains(u))
+            {
+                if (skipped < next)
+                {
+                    skipped++;
+                    continue;
+                }
+                v=u;
+                break;
+            }
+        }
+        Coordinate c;
+        do
+        {
+            c = new Coordinate(r.nextInt(width),r.nextInt(height));
+        }
+        while (gameState.getUsedCoordinates()[c.getX()][c.getY()]!=0);
+        return new GameMove(v,c);
+
     }
     private GameMove selectMove(GameMove lastMove) {
         // First: Apply the last move by the opponent if there is one.
@@ -137,119 +186,105 @@ public class DistancePlayer implements NewPlayer {
         int numHorizontalPartitions = 50;
         int numVerticalPartitions = 50;
 
+        GameMove bestMove = randomMove(lastMove);
+
+        //double minMaxDistance = 0;
+        //minMaxDistance = (role == MAX) ? Double.MIN_VALUE : Double.MAX_VALUE;
+
+        double minMaxCrossings = 0;
+        minMaxCrossings = (role == MAX) ? Double.MIN_VALUE : Double.MAX_VALUE;
+
         // if there is no placed vertex choose at random
         if (gameState.getPlacedVertices().isEmpty()){
-            GameMove randomMove = randomMove(lastMove);
-            gameState.applyMove(randomMove);
-            return randomMove;
+            gameState.applyMove(bestMove);
+            return bestMove;
         }
 
-        int numNeighbors = 2;
-        // get numNeighbors Neighbors
-        HashMap<Vertex,List<Vertex>> neighbors = new HashMap<>();
-        neighbors = getNeighborsOfPlacedVertices();
-        neighbors.size();
+        // Set of possible coordinates to place a vertex.
+        HashSet<Coordinate> candidateCoordinates = sampleRandomGrid(width, height, numHorizontalPartitions,numVerticalPartitions);
+        // the number of coordinates to be compared for each candidate vertex
+        int numCoordinatesToCompare = 2;
 
-        //GameMove move = new GameMove(bestVertex, bestCoordinate);
-        //gameState.applyMove(move);
-        //return move;
-
-
-
-
-
-
-
-        Iterator<Vertex> placedVerticesIterator = gameState.getPlacedVertices().iterator();
-        int i = 0;
-        while ( i < numNeighbors ){
-            if (!placedVerticesIterator.hasNext()){ break;}
-            Vertex placedVertex = placedVerticesIterator.next();
-            Iterable<Edge> incidentEdges = g.getIncidentEdges(placedVertex);
-            List<Vertex> otherNeighbors = new ArrayList<>();
-            for (Edge e: incidentEdges){
-                Vertex s = e.getS();
-                Vertex t = e.getT();
-                if (s.equals(placedVertex) && !gameState.getPlacedVertices().contains(t) ){
-                     otherNeighbors.add(t);
-                     neighbors.put(placedVertex, otherNeighbors);
-                    i++;
-                } else if (t.equals(placedVertex) && !gameState.getPlacedVertices().contains(s) ){
-                    otherNeighbors.add(s);
-                    neighbors.put(placedVertex, otherNeighbors);
-                    i++;
-                }
+        HashMap<Vertex,List<Vertex>> freeNeighborsOfPlacedVertices = new HashMap<>();
+        freeNeighborsOfPlacedVertices = getFreeNeighborsOfPlacedVertices();
+        Iterator<Map.Entry<Vertex,List<Vertex>>> iterator = freeNeighborsOfPlacedVertices.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<Vertex,List<Vertex>> entry = iterator.next();
+            Vertex         placedVertex = entry.getKey();
+            List<Vertex> freeNeighbours = entry.getValue();
+            for (Vertex freeNeighbour : freeNeighbours){
+                HashMap<Coordinate, Double> distanceToCandidateCoordinates = getDistances(placedVertex, candidateCoordinates);
+                // Get numCoordinatesToCompare many coordinates that have the maximum/minimum distance to the placed coordinate
+                HashMap<Coordinate,Double> minMaxDistanceOfCoordinates = getMinMaxDistanceOfCoordinates(distanceToCandidateCoordinates, numCoordinatesToCompare);
+                // Find best coordinate by comparing the crossing number between the candidates
+                GameMove move = getMinMaxCrossingsOfCoordinates(minMaxDistanceOfCoordinates.keySet(), freeNeighbour);
+                gameState.applyMove(move);
+                return move;
             }
         }
-        //System.out.println("neighbours: "+ neighbors.keySet());
-        HashSet<Coordinate> randomGridCoordinates = sampleRandomGrid(width, height, numHorizontalPartitions, numVerticalPartitions, gameState.getUsedCoordinates());
-        NewRandomPlayer newRandomPlayer = new NewRandomPlayer("rand");
-        GameMove bestMove = switch(role){
-            case MAX -> newRandomPlayer.maximizeCrossings(lastMove);
-            case MIN -> newRandomPlayer.minimizeCrossings(lastMove);
-        };
-        int minMaxNumCrossings = role == MAX ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        // n root neighbours
-        for (Vertex n : neighbors.keySet()){
-            List<Vertex> neighborsList = neighbors.get(n);
-            Coordinate placedCoordinate = gameState.getVertexCoordinates().get(n);
-            double maxMinDistance = role == MAX ? Double.MIN_VALUE : Double.MAX_VALUE;
-            HashMap<Vertex, Coordinate> vertexDistanceToNeighbor = new HashMap<>();
-            // test each grid coordinate for shortest longest distance to root vertex
-            //Coordinate coordinateMaxMinDistanceToRoot;
-            for (Coordinate c : randomGridCoordinates){
-                double distance = distance(placedCoordinate, c);
-                if (role == MAX  && distance > maxMinDistance){
-                    maxMinDistance = distance;
-                    //coordinateMaxMinDistanceToRoot = c;
-                    bestMove = new GameMove(neighborsList.get(0),c);
-                } else if (role == MIN && distance < maxMinDistance) {
-                    maxMinDistance = distance;
-                    //coordinateMaxMinDistanceToRoot = c;
-                    bestMove = new GameMove(neighborsList.get(0),c);
-                }
-            }
-
-
-            // find num crossings for each vertex that is closest/farthest. Select move depending on min/max crossing num
-            //for (Vertex v : neighborsList){
-            //    Coordinate c_temp = coordinateMaxMinDistanceToRoot;
-            //    vertexCoordinates.put(v,c_temp);
-            //    Graph gPrime = graphWithPlacedVertices(g, vertexCoordinates);
-            //    CrossingCalculator cc = new CrossingCalculator(gPrime, vertexCoordinates);
-//
-            //    int numCrossings = cc.computeCrossingNumber();
-//
-            //    System.out.println("t");
-            //    vertexCoordinates.remove(v,c_temp);
-//
-            //    if (role == MAX && numCrossings > minMaxNumCrossings){
-            //        minMaxNumCrossings = numCrossings;
-            //        bestMove = new GameMove(v,c_temp);
-            //    } else if (role == MIN && numCrossings < minMaxNumCrossings) {
-            //        minMaxNumCrossings = numCrossings;
-            //        bestMove = new GameMove(v,c_temp);
-            //    }
-
-            //}
-
-            // for each neighbour of a root neighbour
-            //for (Vertex v : neighborsList){
-            //    // test each grid coordinate for shortest longest distance
-            //    for (Coordinate c : randomGridCoordinates){
-            //        double distance = distance(placedCoordinate, c);
-            //        if (role == MAX && distance > maxMinDistance){
-            //            maxMinDistance = distance;
-            //            vertexDistanceToNeighbor.put(v,c);
-            //        } else if (role == MIN && distance > maxMinDistance) {
-            //            maxMinDistance = distance;
-            //            vertexDistanceToNeighbor.put(v,c);
-            //        }
-            //    }
-            //}
-
-        }
+        gameState.applyMove(bestMove);
         return bestMove;
+    }
+
+    private GameMove getMinMaxCrossingsOfCoordinates(Set<Coordinate> coordinates, Vertex freeNeighbour) {
+        int minOrMax = role.equals(MAX) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        Coordinate candidate = null;
+        for (Coordinate c : coordinates) {
+            //place vertex
+            gameState.getVertexCoordinates().put(freeNeighbour, c);
+
+            //calculate new crossing number
+            Graph gPrime = graphWithPlacedVertices(g, gameState.getVertexCoordinates());
+            BentleyOttmannCrossingCalculatorLite cc = new BentleyOttmannCrossingCalculatorLite(gPrime, gameState.getVertexCoordinates());
+            int numCrossings = cc.calculate();
+            if ((role.equals(MAX) && numCrossings > minOrMax) || (role.equals(MIN) && numCrossings < minOrMax)) {
+                minOrMax = numCrossings;
+                candidate = c;
+            }
+
+            //remove placed vertex to reset game state
+            gameState.getVertexCoordinates().remove(freeNeighbour, c);
+        }
+
+        return new GameMove(freeNeighbour, candidate);
+    }
+
+    /**
+     *
+     * @param distanceToCandidateCoordinates already computed distance from placed neighbour for coordinate
+     * @param numCoordinatesToCompare what the name imply
+     * @return numCoordinatesToCompare many coordinates, that are closest for MIN and farthest away from neighbour for MAX
+     */
+    private HashMap<Coordinate, Double> getMinMaxDistanceOfCoordinates(HashMap<Coordinate, Double> distanceToCandidateCoordinates, int numCoordinatesToCompare) {
+        // Create a list from the entries of the input map
+        List<Map.Entry<Coordinate, Double>> entryList = new ArrayList<>(distanceToCandidateCoordinates.entrySet());
+
+        // Sort the list using a comparator based on values
+        entryList.sort((role.equals(MAX) ? Map.Entry.comparingByValue() : Map.Entry.<Coordinate, Double>comparingByValue().reversed()));
+        // Create a new map to store the result
+        HashMap<Coordinate, Double> result = new HashMap<>();
+
+        // Copy the desired number of entries based on the role
+        for (int i = 0; i < numCoordinatesToCompare && i < entryList.size(); i++) {
+            Map.Entry<Coordinate, Double> entry = entryList.get(i);
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+    /**
+     * @param placedVertex vertex from which distance is measured
+     * @param candidateCoordinates positions to which the distance is measured
+     * @return set of distances for each candidate coordinates to the already placed vertex
+     */
+    private HashMap<Coordinate, Double> getDistances(Vertex placedVertex, HashSet<Coordinate> candidateCoordinates) {
+        HashMap<Coordinate, Double> distanceToCandidateCoordinates = new HashMap<>();
+        for (Coordinate candidateCoordinate : candidateCoordinates){
+            Coordinate placedVertexCoordinate = gameState.getVertexCoordinates().get(placedVertex);
+            distanceToCandidateCoordinates.put(candidateCoordinate, distance(candidateCoordinate,placedVertexCoordinate));
+        }
+        return distanceToCandidateCoordinates;
     }
 
     /**
